59c9ec3725ea19e9362a3c77dfd6a07460fa3de4
kovacsmate88 <kovacsmate321@gmail.com>
2023-06-22 13:34:49 +0200

    created a singel python web app that gives back the name of the host

---
+++ app.py
@@ -0,0 +1,15 @@
+import socket
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_host():
+    hostname = socket.gethostname()
+    return f"Thename of the host server is: {hostname}\n"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
