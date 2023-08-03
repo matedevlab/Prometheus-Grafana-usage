4568207dc9ecd45127f80cea6ae7209a76ff43de
kovacsmate88 <kovacsmate321@gmail.com>
2023-08-03 13:10:02 +0200

    add metrics route to expose metrics in a format that Prometheus can scrape and collect

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 import socket
 
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
+
+requests_counter = Counter("myapp_requests_total", "Total number of requests")
 
 
 @app.route("/")  # this endpoint will prometheus watches
@@ -15,7 +18,17 @@ def get_host():
         print(f"Error retrieving hostname: {e}")
 
 
+@app.route("/metrics")
+def metrics():
+    # Increment the counter metric for each request
+    requests_counter.inc()
+
+    # Generate Prometheus metrics output
+    return Response(generate_latest(), content_type="text/plain")
+
+
 if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", 5000))
+
     app.run(host=host, port=port)
