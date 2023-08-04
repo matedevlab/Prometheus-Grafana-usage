964c18517812f5cf3a1e7c9cec4559904e56fb30
kovacsmate88 <kovacsmate321@gmail.com>
2023-08-04 12:31:48 +0200

    integrated the metrics route  into the main route

--- app.py
+++ app.py
@@ -9,22 +9,24 @@ app = Flask(__name__)
 requests_counter = Counter("myapp_requests_total", "Total number of requests")
 
 
-@app.route("/")  # this endpoint will prometheus watches
+@app.route("/")
 def get_host():
     try:
+        # Increment the counter metric for each request
+        requests_counter.inc()
+
+        # Generate Prometheus metrics output
+        metrics_output = generate_latest()
+
+        # Concatenate the hostname and metrics output
         hostname = socket.gethostname()
-        return f"The name of the host server is: {hostname}\n"
+        combined_output = (
+            f"The name of the host server is: {hostname}\n\n{metrics_output}"
+        )
+
+        return Response(combined_output, content_type="text/plain")
     except socket.error as e:
         print(f"Error retrieving hostname: {e}")
-
-
-@app.route("/metrics")
-def metrics():
-    # Increment the counter metric for each request
-    requests_counter.inc()
-
-    # Generate Prometheus metrics output
-    return Response(generate_latest(), content_type="text/plain")
 
 
 if __name__ == "__main__":
